7e2c1719221d50b711605a287a3869e1c0713008
msneddon <mwsneddon@lbl.gov>
2015-07-06 16:03:53 -0700

    added more basic things

---
+++ test/script_test/basic_test.py
@@ -0,0 +1,35 @@
+
+import unittest
+import json
+
+from pprint import pprint
+
+from subprocess import call
+
+class TestCoreGenomeUtilMethods(unittest.TestCase):
+
+  def test_method(self):
+    print("\n\n----------- basic test ----------")
+
+    # write the 
+
+
+
+    # call the script with some input
+    out = call(["run_KBaseCoreGenomeUtil.sh", 
+       "test/script_test/input.json", 
+       "test/script_test/output.json", 
+       "test/script_test/token.txt"])
+
+    # print stdout of implementation
+    print(out);
+
+    # read and print output of the function
+    with open('test/script_test/output.json') as o:    
+        output = json.load(o)
+    pprint(output)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
