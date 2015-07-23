2d613fc93a434068dedb345278081c5b9c2baf2a
plane83 <root@fei-h11.novalocal>
2015-07-23 20:45:43 +0000

    update Imp file

--- lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
+++ lib/biokbase/genome_util/KBaseGenomeUtilImpl.py
@@ -1,5 +1,6 @@
 #BEGIN_HEADER
 
+import simplejson
 import sys
 import os
 import glob
@@ -9,7 +10,7 @@ from pprint import pprint
 import script_util
 #from biokbase.workspace.client import Workspace
 #from workspace.client import Workspace
-
+import biokbase.workspace.client
 
 #END_HEADER
 
@@ -44,11 +45,11 @@ class KBaseGenomeUtil:
         print "SCRIPT CONFIG:"
         pprint(config)
         if 'ws_url' in config:
-        	__WS_URL = config['ws_url']
-        	print "Setting __WS_URL = "+__WS_URL
+              __WS_URL = config['ws_url']
+              print "Setting __WS_URL = "+__WS_URL
         if 'temp_dir' in config:
-        	__TEMP_DIR = config['temp_dir']
-        	print "Setting __TEMP_DIR = "+__TEMP_DIR
+              __TEMP_DIR = config['temp_dir']
+              print "Setting __TEMP_DIR = "+__TEMP_DIR
 
         #END_CONSTRUCTOR
         pass
@@ -57,98 +58,119 @@ class KBaseGenomeUtil:
         # ctx is the context object
         # return variables are: returnVal
         #BEGIN blast_against_genome
+      
+        #print "start"
+        if len(params['query']) > 5:
+            sequence=params['query']
+        else:
+            #error message: your sequence are too short
+            raise Exception("The input sequence is too short!")
+        #else:
+            #sequence=script_util.get_seq(params['gene_id'])
+            #sequence=(params['gene_id'])
+
+
+      
+        #print "generate input file for query sequence\n"
+        target=open('tmp_seq','w')
+        target.write(">")
+        target.write("input_seq\n")
+        target.write(sequence)
+        target.close()
+      
+        f=open("test/script_test/token.txt",'r')
+        token=f.read()
+	f.close()
+        #print token
+        print "downloading genome object from workspace\n"
+	genome=script_util.get_genome(params['genome_ids'][0],params['ws_id'],token)
+        print "finished downloading\n";
+
+        if os.path.exists('blast_db'):
+            files=glob.glob('blast_db/*')
+            for f in files: os.remove(f)
+        if not os.path.exists('blast_db'): os.makedirs('blast_db')
+        
+	with open('tmp_data','w') as outfile:
+            json.dump(genome, outfile)      
+      
+        check_seq=0
+        if(params['blast_program'] == 'blastp'):
+            formatdb_type='T'
+            #extract protein sequences from the genome object
+            res1=open('tmp_data').read()
+            res=json.loads(res1)
+            target=open('blast_db/tmp_genome_fasta','w')
+            for gene in res['data']['features']:
+                  if 'protein_translation' in gene.keys():
+                        target.write(">" + gene['id'] + "\n" + gene['protein_translation'] + "\n")
+                        check_seq=1
+            target.close()
+      
+      
+        if(params['blast_program'] == 'blastn'):
+            formatdb_type='F'
+            #extract dna sequence from the genome object
+            res1=open('tmp_data').read()
+            res=json.loads(res1)
+            target=open('blast_db/tmp_genome_fasta','w')
+            for gene in res['data']['features']:
+                  if 'dna_sequence' in gene.keys():
+                        target.write(">" + gene['id'] + "\n" + gene['dna_sequence'] + "\n")
+                        check_seq=1
+            target.close()
+
+        if check_seq == 0:
+            raise Exception("The genome object does not contain any sequences!")
+      
+      
+      
+        os.remove('tmp_data')
+      
+        print "formatdb..\n"
+        #format database for blast
+      
+        cmdstring="formatdb -i blast_db/tmp_genome_fasta -p %s" %(formatdb_type)
+        os.system(cmdstring)
+
+        #blast search
+        cmdstring="blastall -p %s -i tmp_seq -m 9 -o tmp_out -d blast_db/tmp_genome_fasta -e %s" % (params['blast_program'], params['e-value'])
+      
+        os.system(cmdstring)
+        os.remove('tmp_seq')
+      
+        #extract the blast output
+        res=script_util.extract_blast_output('tmp_out')
+        os.remove('tmp_out')
 	
-	print "start"
-	if len(params['query']) > 5:
-		sequence=params['query']
-	else:
-		#error message: your sequence are too short
-		raise Exception("The input sequence is too short!")
-	#else:
-		#sequence=script_util.get_seq(params['gene_id'])
-		#sequence=(params['gene_id'])
+	res1={}
+	res1['hits']=res
+	print "finished!!!"
+	print res1
+      
+
+        workspace_service_url="https://ci.kbase.us/services/ws/"
+        workspaceClient = biokbase.workspace.client.Workspace(url=workspace_service_url, token=token)
 	
 
-	genome_id='Bifidobacterium_animalis_subsp._lactis_AD011'
-	workspaceid='plane83:1436884411390'
-	
-	#print "generate input file for query sequence\n"
-	target=open('tmp_seq','w')
-	target.write(">")
-	target.write("input_seq\n")
-	target.write(sequence)
-	target.close()
-	
+	#store the BLAST output back into workspace
+        res= workspaceClient.save_objects(
+        {"workspace":params['ws_id'],
+        "objects": [{
+            #"type":"KBaseBiochem.Media-1.0",
+            "type":"GenomeUtil.BLAST_output-0.1",
+            "data":res1,
+            "name":params['output_name']}
+        ]})
+        
 
-	#print "downloading genome object from workspace\n"
-	#genome=script_util.get_genome('genome_id','workspaceid',ctx['token'])
-	#print "finished downloading\n";
-
-	if os.path.exists('blast_db'):
-		files=glob.glob('blast_db/*')
-		for f in files: os.remove(f)
-	if not os.path.exists('blast_db'): os.makedirs('blast_db')
-	#with open('tmp_data','w') as outfile:
-	#	json.dump(genome, outfile)	
-	
-
-	check_seq=0
-	if(params['blast_program'] == 'blastp'):
-		formatdb_type='T'
-		#extract protein sequences from the genome object
-		res1=open('tmp_data').read()
-		res=json.loads(res1)
-		target=open('blast_db/tmp_genome_fasta','w')
-		for gene in res['data']['features']:
-			if 'protein_translation' in gene.keys():
-				target.write(">" + gene['id'] + "\n" + gene['protein_translation'] + "\n")
-				check_seq=1
-		target.close()
-	
-	
-	if(params['blast_program'] == 'blastn'):
-		formatdb_type='F'
-		#extract dna sequence from the genome object
-		res1=open('tmp_data').read()
-		res=json.loads(res1)
-		target=open('blast_db/tmp_genome_fasta','w')
-		for gene in res['data']['features']:
-			if 'dna_sequence' in gene.keys():
-				target.write(">" + gene['id'] + "\n" + gene['dna_sequence'] + "\n")
-				check_seq=1
-		target.close()
-
-	if check_seq == 0:
-		raise Exception("The genome object does not contain any sequences!")
-	
-	
-	
-	#os.remove('tmp_data')
-	
-	#print "formatdb..\n"
-	#format database for blast
-	
-	cmdstring="formatdb -i blast_db/tmp_genome_fasta -p %s" %(formatdb_type)
-	os.system(cmdstring)
-
-	#blast search
-	cmdstring="blastall -p %s -i tmp_seq -m 9 -o tmp_out -d blast_db/tmp_genome_fasta -e %s" % (params['blast_program'], params['e-value'])
-	os.system(cmdstring)
-	os.remove('tmp_seq')
-	
-	#extract the blast output
-	res=script_util.extract_blast_output('tmp_out')
-	os.remove('tmp_out')
-	res1=json.loads(res)
-	
-	print "finished"
-	returnVal = res1
+        returnVal = res1
         #END blast_against_genome
 
         # At some point might do deeper type checking...
-        if not isinstance(returnVal, list):
+        if not isinstance(returnVal, dict):
             raise ValueError('Method blast_against_genome return value ' +
-                             'returnVal is not type list as required.')
+                             'returnVal is not type dict as required.')
         # return the results
         return [returnVal]
 
